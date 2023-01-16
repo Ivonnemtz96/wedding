3cc8fa43f6ebcaec568e8d8d62987a753f2b0937
Ivonnemtz96 <ivonnemtz96@outlook.es>
2023-01-16 16:34:46 -0700

    Footer cambio de redes sociales

--- includes/footer.php
+++ includes/footer.php
@@ -7,9 +7,9 @@
         <h2>Thank you</h2>
         <ul class="social-links">
             <li><a href="#"><i class="ti-facebook"></i></a></li>
-            <li><a href="#"><i class="ti-twitter-alt"></i></a></li>
-            <li><a href="#"><i class="ti-linkedin"></i></a></li>
-            <li><a href="#"><i class="ti-pinterest"></i></a></li>
+            <!-- <li><a href="#"><i class="ti-twitter-alt"></i></a></li> -->
+            <li><a href="#"><i class="ti-instagram"></i></a></li>
+            <!-- <li><a href="#"><i class="ti-pinterest"></i></a></li> -->
         </ul>
         <p>Copyright 2023 <strong>Weddings at Cabo</strong>, Made with love by <a href="http://banangroup.mx"><strong>Banana Group Marketing</strong></a></p>
     </div>
