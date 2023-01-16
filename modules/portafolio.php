e2451da855d531a44b42a88b690389b479cea26a
Ivonnemtz96 <ivonnemtz96@outlook.es>
2023-01-16 14:57:45 -0700

    Cambio fotos en portafolio, portafolio nuevo

--- modules/portafolio.php
+++ modules/portafolio.php
@@ -21,7 +21,7 @@
 </section>
 <!-- end page-title -->
 <!-- start invitation-section -->
-<section class="invitation-section section-padding" style="padding-bottom: 0;">
+<section hidden class="invitation-section section-padding" style="padding-bottom: 0;">
     <div class="container-fluid">
         <div class="row">
             <div class="col col-xs-12">
@@ -33,4 +33,75 @@
             </div>
         </div>
     </div> <!-- end container -->
+</section>
+
+<section class="portafolio">
+
+    <div class="container" >
+        <div class="row">
+            <!-- 3 Columns -->
+            <div class="col-xs-4 col-md-4 col-lg-4" style="margin-bottom: 45px; margin-top:20px">
+                <div class="image">
+                    <img class="img-responsive"
+                        src="/assets/images/1.webp"
+                        alt="baby-tigger">
+                    <h2 id="caption"><span>baby tiger</span></h2>
+                </div>
+            </div>
+            <div class="col-xs-4 col-md-4 col-lg-4" style="margin-bottom: 45px; margin-top:20px">
+                <div class="image">
+                    <img class="img-responsive"
+                        src="/assets/images/2.webp"
+                        alt="baby-tigger">
+                    <h2 id="caption"><span>baby tiger</span></h2>
+                </div>
+            </div>
+            <div class="col-xs-4 col-md-4 col-lg-4" style="margin-bottom: 45px; margin-top:20px">
+                <div class="image">
+                    <img class="img-responsive"
+                        src="/assets/images/3.webp"
+                        alt="baby-tigger">
+                    <h2 id="caption"><span>baby tiger</span></h2>
+                </div>
+            </div>
+             <!-- 2 Columns -->
+             <div class="col-xs-6 col-md-6 col-lg-6" style="margin-bottom: 45px; margin-top:20px">
+                <div class="image">
+                    <img class="img-responsive"
+                        src="/assets/images/6.webp"
+                        alt="baby-tigger">
+                    <h2 id="caption"><span>baby tiger</span></h2>
+                </div>
+            </div>
+            <div class="col-xs-6 col-md-6 col-lg-6" style="margin-bottom: 45px; margin-top:20px">
+                <div class="image">
+                    <img class="img-responsive"
+                        src="/assets/images/7.webp"
+                        alt="baby-tigger">
+                    <h2 id="caption"><span>baby tiger</span></h2>
+                </div>
+            </div>
+
+            <!-- 3 Columns -->
+            <div class="col-xs-4 col-md-4 col-lg-4" style="margin-bottom: 45px; margin-top:20px">
+                <div class="image">
+                    <img class="img-responsive"
+                        src="/assets/images/4.webp"
+                        alt="baby-tigger">
+                    <h2 id="caption"><span>baby tiger</span></h2>
+                </div>
+            </div>
+            <div class="col-xs-4 col-md-4 col-lg-4" style="margin-bottom: 45px; margin-top:20px">
+                <div class="image">
+                    <img class="img-responsive"
+                        src="/assets/images/5.webp"
+                        alt="baby-tigger">
+                    <h2 id="caption"><span>baby tiger</span></h2>
+                </div>
+            </div>
+           
+
+        </div>
+
+    </div>
 </section>
