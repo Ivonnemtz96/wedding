32ba64167079551853943dae43c0714758a11545
Ivonnemtz96 <ivonnemtz96@outlook.es>
2023-01-16 15:20:44 -0700

    Portafolio ok

--- modules/portafolio.php
+++ modules/portafolio.php
@@ -36,70 +36,65 @@
 </section>
 
 <section class="portafolio">
-
-    <div class="container" >
+    <div class="container" style="margin-top: 5rem;">
+        <div class="row">
+            <div class="col col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
+                <div class="section-title">
+                    <h2>OUR AMAZING WORK</h2>
+                    <!-- <p>
+                        The best moments captured in one single image
+                    </p> -->
+                </div>
+            </div>
+        </div>
         <div class="row">
             <!-- 3 Columns -->
             <div class="col-xs-4 col-md-4 col-lg-4" style="margin-bottom: 45px; margin-top:20px">
                 <div class="image">
-                    <img class="img-responsive"
-                        src="/assets/images/1.webp"
-                        alt="baby-tigger">
-                    <h2 id="caption"><span>baby tiger</span></h2>
+                    <img class="img-responsive" src="/assets/images/1.webp" alt="baby-tigger">
+                    <h2 id="caption"><span><a href="/detalles">Keana & Taran</a></span></h2>
                 </div>
             </div>
             <div class="col-xs-4 col-md-4 col-lg-4" style="margin-bottom: 45px; margin-top:20px">
                 <div class="image">
-                    <img class="img-responsive"
-                        src="/assets/images/2.webp"
-                        alt="baby-tigger">
-                    <h2 id="caption"><span>baby tiger</span></h2>
+                    <img class="img-responsive" src="/assets/images/2.webp" alt="baby-tigger">
+                    <h2 id="caption"><span><a href="/detalles">Briana & Sean</a></span></h2>
                 </div>
             </div>
             <div class="col-xs-4 col-md-4 col-lg-4" style="margin-bottom: 45px; margin-top:20px">
                 <div class="image">
-                    <img class="img-responsive"
-                        src="/assets/images/3.webp"
-                        alt="baby-tigger">
-                    <h2 id="caption"><span>baby tiger</span></h2>
+                    <img class="img-responsive" src="/assets/images/3.webp" alt="baby-tigger">
+                    <h2 id="caption"><span><a href="/detalles">Lauren & Octavio</a></span></h2>
                 </div>
             </div>
-             <!-- 2 Columns -->
-             <div class="col-xs-6 col-md-6 col-lg-6" style="margin-bottom: 45px; margin-top:20px">
+            <!-- 2 Columns -->
+            <div class="col-xs-6 col-md-6 col-lg-6" style="margin-bottom: 45px; margin-top:20px">
                 <div class="image">
-                    <img class="img-responsive"
-                        src="/assets/images/6.webp"
-                        alt="baby-tigger">
-                    <h2 id="caption"><span>baby tiger</span></h2>
+                    <img class="img-responsive" src="/assets/images/6.webp" alt="baby-tigger">
+                    <h2 id="caption"><span><a href="/detalles">Sara & Steven</a></span></h2>
                 </div>
             </div>
             <div class="col-xs-6 col-md-6 col-lg-6" style="margin-bottom: 45px; margin-top:20px">
                 <div class="image">
-                    <img class="img-responsive"
-                        src="/assets/images/7.webp"
-                        alt="baby-tigger">
-                    <h2 id="caption"><span>baby tiger</span></h2>
+                    <img class="img-responsive" src="/assets/images/7.webp" alt="baby-tigger">
+                    <h2 id="caption"><span><a href="/detalles">Amber & Franklin</a></span></h2>
                 </div>
             </div>
 
             <!-- 3 Columns -->
             <div class="col-xs-4 col-md-4 col-lg-4" style="margin-bottom: 45px; margin-top:20px">
                 <div class="image">
-                    <img class="img-responsive"
-                        src="/assets/images/4.webp"
-                        alt="baby-tigger">
-                    <h2 id="caption"><span>baby tiger</span></h2>
+                    <img class="img-responsive" src="/assets/images/4.webp" alt="baby-tigger">
+                    <h2 id="caption"><span><a href="/detalles">Amera & Ben</a></span></h2>
                 </div>
             </div>
             <div class="col-xs-4 col-md-4 col-lg-4" style="margin-bottom: 45px; margin-top:20px">
                 <div class="image">
-                    <img class="img-responsive"
-                        src="/assets/images/5.webp"
-                        alt="baby-tigger">
-                    <h2 id="caption"><span>baby tiger</span></h2>
+                    <img class="img-responsive" src="/assets/images/5.webp" alt="baby-tigger">
+                    <h2 id="caption"><span><a href="/detalles">Gaby & Brandon</a></span></h2>
                 </div>
             </div>
-           
+
 
         </div>
 
