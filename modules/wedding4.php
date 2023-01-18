3fb4294e08cb28d4ea94d1ce1f9d5284f8f2f5d2
Ivonnemtz96 <ivonnemtz96@outlook.es>
2023-01-18 11:44:57 -0700

    Wedding 4 ok

--- modules/wedding4.php
+++ modules/wedding4.php
@@ -5,7 +5,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col col-xs-12">
-                        <h2>Sara & Steven</h2>
+                        <h2>Tara & Logan</h2>
                     </div>
                 </div> <!-- end row -->
             </div> <!-- end container -->
@@ -27,64 +27,45 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6">
-                <img src="/assets/images/detalles/1.webp" alt>
+                <img src="/assets/images/detalles/13.webp" alt>
             </div>
             <div class="col-md-6">
                 <div class="blog-content">
                     <div class="post format-standard-image">
-                        <div class="entry-media">
-
-                        </div>
-                        <ul class="entry-meta">
-                            <li><a href="#"><i class="ti-time"></i> 20 Apr, 2019</a></li>
-                            <li><a href="#"><i class="ti-book"></i> Wedding Planning</a></li>
-                        </ul>
-                        <!-- A orilla de la playa, acompañados del atardecer -->
-                        <h2>At the edge of the beach, accompanied by the sunset</h2>
+                        <h2>Tara & Logan</h2>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis asperiores quibusdam quidem,
-                            nulla inventore quo quaerat esse. Laborum repellendus ab numquam perspiciatis vel illum, aut
-                            a explicabo. Doloremque, mollitia ipsum!
+                            Loved working with this sweet and beautiful couple. Having the
+                            opportunity to meet them before the wedding, we realized there was great
+                            chemistry from the beginning. They were not only beautiful outside, but
+                            beautiful inside too with a huge heart. Tara was looking for unique details
+                            and we achieved a perfect balance in the design and colors of her
+                            wedding. Truly a pleasure for me to work with them!
                         </p>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima doloribus debitis autem,
-                            nostrum et quis cupiditate corporis sapiente qui magni! Numquam earum, quam facilis saepe
-                            ullam deserunt atque aliquam corrupti?
-                        </p>
-                        <h3>There is no instinct like that of the heart</h3>
-                        <p>
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias assumenda exercitationem
-                            beatae dolores cumque esse quisquam vitae, harum fugit! Repellat error maxime asperiores
-                            inventore quia ipsa expedita laboriosam iste. Expedita corrupti voluptas aut minus amet
-                            reprehenderit impedit, numquam repellat fugiat!
-                        </p>
-
-
                     </div>
-                    <div class="container listas">
-                        <ul>
-                            <li>Cliente: <span>Sara & Steven</span></li>
-                            <li>Location: <span>San Jose del Cabo</span></li>
-                            <li>Date: <span>20 Apr 2019</span></li>
-                            <li>Duration: <span>3 Month</span></li>
-                            <li>Tag: <span>Wedding, Planning</span></li>
-                        </ul>
-                    </div>
-
-                    <!-- end comments-area -->
                 </div>
             </div>
         </div>
 
         <div class="row" style="margin-top: 4rem;">
-            <div class="col-md-4">
-                <img src="/assets/images/detalles/2.webp" alt>
+            <div class="col-md-4" style="margin-top: 2rem; margin-bottom: 2rem;">
+                <img src="/assets/images/detalles/14.webp" alt>
             </div>
-            <div class="col-md-4">
-                <img src="/assets/images/detalles/3.webp" alt>
+            <div class="col-md-4" style="margin-top: 2rem; margin-bottom: 2rem;">
+                <img src="/assets/images/detalles/15.webp" alt>
             </div>
-            <div class="col-md-4">
-                <img src="/assets/images/detalles/4.webp" alt>
+            <div class="col-md-4" style="margin-top: 2rem; margin-bottom: 2rem;">
+                <img src="/assets/images/detalles/16.webp" alt>
+            </div>
+
+
+            <div class="col-md-4" style="margin-top: 2rem; margin-bottom: 2rem;">
+                <img src="/assets/images/detalles/17.webp" alt>
+            </div>
+            <div class="col-md-4" style="margin-top: 2rem; margin-bottom: 2rem;">
+                <img src="/assets/images/detalles/19.webp" alt>
+            </div>
+            <div class="col-md-4" style="margin-top: 2rem; margin-bottom: 2rem;">
+                <img src="/assets/images/detalles/18.webp" alt>
             </div>
         </div>
     </div> <!-- end container -->
