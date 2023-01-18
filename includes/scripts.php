f85e57af9841fd414504fbe75472ddba0bdfb839
Ivonnemtz96 <ivonnemtz96@outlook.es>
2023-01-18 12:13:04 -0700

    Redes sociales ok, se agrega script para font awesome

--- includes/scripts.php
+++ includes/scripts.php
@@ -8,7 +8,8 @@
 <!-- Custom script for this template -->
 <script src="/assets/js/script.js"></script>
 
-
+<!-- font awesome -->
+<script src="https://kit.fontawesome.com/e631eca424.js" crossorigin="anonymous"></script>
 
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
