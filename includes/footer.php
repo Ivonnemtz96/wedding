118a9ca59e5584dc0eaba73b2bb33e8cfd7f4abd
Ivonnemtz96 <ivonnemtz96@outlook.es>
2023-01-18 15:16:35 -0700

    links de redes sociales ok

--- includes/footer.php
+++ includes/footer.php
@@ -209,9 +209,9 @@
         </div>
         <h2>Thank you</h2>
         <ul class="social-links">
-            <li><a href="#"><i class="ti-facebook"></i></a></li>
+            <li><a href="https://www.facebook.com/weddingsatcabo"><i class="ti-facebook"></i></a></li>
             <!-- <li><a href="#"><i class="ti-twitter-alt"></i></a></li> -->
-            <li><a href="#"><i class="ti-instagram"></i></a></li>
+            <li><a href="https://www.instagram.com/weddings_atcabo/"><i class="ti-instagram"></i></a></li>
             <!-- <li><a href="#"><i class="ti-pinterest"></i></a></li> -->
         </ul>
         <p style="text-align: center; font-size: 15px;">Copyright 2023 <strong>Weddings at Cabo</strong>, Made with love by <a
