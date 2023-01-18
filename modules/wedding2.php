eaa8492e3304c457ba2f69f8ccc8cf1fddff0ea7
Ivonnemtz96 <ivonnemtz96@outlook.es>
2023-01-18 11:18:49 -0700

    Wedding 2 ok

--- modules/wedding2.php
+++ modules/wedding2.php
@@ -27,7 +27,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6">
-                <img src="/assets/images/detalles/1.webp" alt>
+                <img src="/assets/images/detalles/20.webp" alt>
             </div>
             <div class="col-md-6">
                 <div class="blog-content">
@@ -35,56 +35,47 @@
                         <div class="entry-media">
 
                         </div>
-                        <ul class="entry-meta">
-                            <li><a href="#"><i class="ti-time"></i> 20 Apr, 2019</a></li>
-                            <li><a href="#"><i class="ti-book"></i> Wedding Planning</a></li>
-                        </ul>
                         <!-- A orilla de la playa, acompañados del atardecer -->
-                        <h2>At the edge of the beach, accompanied by the sunset</h2>
+                        <h2>Sara & Steven</h2>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis asperiores quibusdam quidem,
-                            nulla inventore quo quaerat esse. Laborum repellendus ab numquam perspiciatis vel illum, aut
-                            a explicabo. Doloremque, mollitia ipsum!
+                            Sweet, kind and fun among many other qualities to describe how
+                            incredibly beautiful human beings Sara and Steven are. Love working with
+                            them and their amazing families. Sara´s father created a beautiful
+                            ambiance with the flowers. He designed all the decor and even her
+                            bouquet was created by him. Don´t you love it?
                         </p>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima doloribus debitis autem,
-                            nostrum et quis cupiditate corporis sapiente qui magni! Numquam earum, quam facilis saepe
-                            ullam deserunt atque aliquam corrupti?
-                        </p>
-                        <h3>There is no instinct like that of the heart</h3>
-                        <p>
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias assumenda exercitationem
-                            beatae dolores cumque esse quisquam vitae, harum fugit! Repellat error maxime asperiores
-                            inventore quia ipsa expedita laboriosam iste. Expedita corrupti voluptas aut minus amet
-                            reprehenderit impedit, numquam repellat fugiat!
-                        </p>
-
-
                     </div>
-                    <div class="container listas">
-                        <ul>
-                            <li>Cliente: <span>Sara & Steven</span></li>
-                            <li>Location: <span>San Jose del Cabo</span></li>
-                            <li>Date: <span>20 Apr 2019</span></li>
-                            <li>Duration: <span>3 Month</span></li>
-                            <li>Tag: <span>Wedding, Planning</span></li>
-                        </ul>
-                    </div>
-
                     <!-- end comments-area -->
                 </div>
             </div>
         </div>
 
         <div class="row" style="margin-top: 4rem;">
-            <div class="col-md-4">
-                <img src="/assets/images/detalles/2.webp" alt>
+            <div class="col-md-4" style="margin-top: 2rem; margin-bottom: 2rem;">
+                <img src="/assets/images/detalles/21.webp" alt>
             </div>
-            <div class="col-md-4">
-                <img src="/assets/images/detalles/3.webp" alt>
+            <div class="col-md-4" style="margin-top: 2rem; margin-bottom: 2rem;">
+                <img src="/assets/images/detalles/22.webp" alt>
             </div>
-            <div class="col-md-4">
-                <img src="/assets/images/detalles/4.webp" alt>
+            <div class="col-md-4" style="margin-top: 2rem; margin-bottom: 2rem;">
+                <img src="/assets/images/detalles/23.webp" alt>
+            </div>
+
+            <div class="col-md-4" style="margin-top: 2rem; margin-bottom: 2rem;">
+                <img src="/assets/images/detalles/24.webp" alt>
+            </div>
+            <div class="col-md-4" style="margin-top: 2rem; margin-bottom: 2rem;">
+                <img src="/assets/images/detalles/25.webp" alt>
+            </div>
+            <div class="col-md-4" style="margin-top: 2rem; margin-bottom: 2rem;">
+                <img src="/assets/images/detalles/26.webp" alt>
+            </div>
+
+            <div class="col-md-4" style="margin-top: 2rem; margin-bottom: 2rem;">
+                <img src="/assets/images/detalles/27.webp" alt>
+            </div>
+            <div class="col-md-4" style="margin-top: 2rem; margin-bottom: 2rem;">
+                <img src="/assets/images/detalles/28.webp" alt>
             </div>
         </div>
     </div> <!-- end container -->
