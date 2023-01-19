117fb0190dd07d111c7b4786e9addaf85b85a462
Ivonnemtz96 <ivonnemtz96@outlook.es>
2023-01-19 16:10:19 -0700

    Planning se agrega texto y fotos, se cambia banner

--- modules/planning.php
+++ modules/planning.php
@@ -25,11 +25,36 @@
         <div class="row">
             <div class="col col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
                 <div class="section-title">
-                    <!-- <h2>OUR AMAZING WORK</h2> -->
+                    <h2 style="color: #b6b8a9;">OUR AMAZING WORK</h2>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos doloremque quia consequatur eos
-                        deserunt vel sequi suscipit ut! Tenetur, perferendis. 
+                        This is your time to eat, drink, and be married!
+                        While you enjoy the festivities, we will take care of the details. Our
+                        specialized planner will work every step of the way to make your wedding a
+                        reality. <br>
+                        Feel free to request a free initial consultation. This will help us to make sure
+                        we understand your vision and can work together to make your wedding
+                        dream come true! <br>
+                        We will be there every step of the way helping you to select the perfect
+                        venue, the right vendors and to customize your food and beverage menus.
+                        We are ready to create an unforgettable night for you and your loved ones!
+                        <br><br>
                     </p>
+
+                    <ul>
+                        <strong>SERVICES INCLUDED</strong>
+                        <li>Meetings before the wedding to be used at your leisure.</li>
+                        <li> If you are planning to visit Los Cabos soon, we will help you to create
+                            an agenda that will include: touring locations, vendor meetings, cake
+                            and catering tastings, floral selection, furniture, decor, etc.</li>
+                        <li>Create a detailed timeline for your event</li>
+                        <li>Budget management</li>
+                        <li>Organization of rehearsal dinner, reservations, etc.</li>
+                        <li>Full design and styling concept based on your vision.</li>
+                        <li>Coordinate the wedding party, all wedding day vendors, and
+                            orchestrate the wedding ceremony.
+                        </li>
+
+                    </ul>
                     <!-- <p>
                         The best moments captured in one single image
                     </p> -->
@@ -40,32 +65,73 @@
             <!-- 3 Columns -->
             <div class="col-12 col-md-4 col-lg-4" style="margin-bottom: 45px; margin-top:20px">
                 <div class="image">
-                    <img class="img-responsive" src="/assets/images/servicios/11.jpg" alt="baby-tigger">
+                    <img class="img-responsive" src="/assets/images/servicios/11.jpg?=1" alt="baby-tigger">
                 </div>
             </div>
             <div class="col-12 col-md-4 col-lg-4" style="margin-bottom: 45px; margin-top:20px">
                 <div class="image">
-                    <img class="img-responsive" src="/assets/images/servicios/12.jpg" alt="baby-tigger">
+                    <img class="img-responsive" src="/assets/images/servicios/12.jpg?=1" alt="baby-tigger">
                 </div>
             </div>
             <div class="col-12 col-md-4 col-lg-4" style="margin-bottom: 45px; margin-top:20px">
                 <div class="image">
-                    <img class="img-responsive" src="/assets/images/servicios/13.jpg" alt="baby-tigger">
+                    <img class="img-responsive" src="/assets/images/servicios/13.jpg?=1" alt="baby-tigger">
                 </div>
             </div>
             <!-- 2 Columns -->
             <div class="col-12 col-md-6 col-lg-6" style="margin-bottom: 45px; margin-top:20px">
                 <div class="image">
-                    <img class="img-responsive" src="/assets/images/servicios/14.jpg" alt="baby-tigger">
+                    <img class="img-responsive" src="/assets/images/servicios/14.jpg?=1" alt="baby-tigger">
                 </div>
             </div>
             <div class="col-12 col-md-6 col-lg-6" style="margin-bottom: 45px; margin-top:20px">
                 <div class="image">
-                    <img class="img-responsive" src="/assets/images/servicios/15.jpg" alt="baby-tigger">
+                    <img class="img-responsive" src="/assets/images/servicios/15.jpg?=1" alt="baby-tigger">
                 </div>
             </div>
+
+            <!-- 3 Columns -->
+            <div class="col-12 col-md-4 col-lg-4" style="margin-bottom: 45px; margin-top:20px">
+                <div class="image">
+                    <img class="img-responsive" src="/assets/images/servicios/42.jpg?=1" alt="baby-tigger">
+                </div>
+            </div>
+            <div class="col-12 col-md-4 col-lg-4" style="margin-bottom: 45px; margin-top:20px">
+                <div class="image">
+                    <img class="img-responsive" src="/assets/images/servicios/43.jpg?=1" alt="baby-tigger">
+                </div>
+            </div>
+            <div class="col-12 col-md-4 col-lg-4" style="margin-bottom: 45px; margin-top:20px">
+                <div class="image">
+                    <img class="img-responsive" src="/assets/images/servicios/44.jpg?=1" alt="baby-tigger">
+                </div>
+            </div>
+            <!-- 2 Columns -->
+            <div class="col-12 col-md-6 col-lg-6" style="margin-bottom: 45px; margin-top:20px">
+                <div class="image">
+                    <img class="img-responsive" src="/assets/images/servicios/45.jpg?=1" alt="baby-tigger">
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-6" style="margin-bottom: 45px; margin-top:20px">
+                <div class="image">
+                    <img class="img-responsive" src="/assets/images/servicios/46.jpg?=1" alt="baby-tigger">
+                </div>
+            </div>
+
+            <!-- 3 Columns -->
+            <div class="col-12 col-md-4 col-lg-4" style="margin-bottom: 45px; margin-top:20px">
+                <div class="image">
+                    <img class="img-responsive" src="/assets/images/servicios/47.jpg?=1" alt="baby-tigger">
+                </div>
+            </div>
+            <div class="col-12 col-md-4 col-lg-4" style="margin-bottom: 45px; margin-top:20px">
+                <div class="image">
+                    <img class="img-responsive" src="/assets/images/servicios/48.jpg?=1" alt="baby-tigger">
+                </div>
+            </div>
+            
+
         </div>
 
     </div>
 </section>
-
