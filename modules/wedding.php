2ea46c939d57d690b472c063e90f2ab6b27a54b8
Ivonnemtz96 <ivonnemtz96@outlook.es>
2023-01-19 16:51:01 -0700

    Sección de weddings ok, fotos actualizadas y textos

--- modules/wedding.php
+++ modules/wedding.php
@@ -24,11 +24,15 @@
     <div class="container" style="margin-top: 5rem;">
         <div class="row">
             <div class="col col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
-                <div class="section-title">
-                    <!-- <h2>OUR AMAZING WORK</h2> -->
+                <div class="section-title portafolio">
+                    <h2 style="color: #b6b8a9;">Wedding Ceremonies</h2>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos doloremque quia consequatur eos
-                        deserunt vel sequi suscipit ut! Tenetur, perferendis.
+                        Los Cabos is becoming increasingly popular as a destination to get
+                        married, and given the opportunity to get married surrounded by stunning
+                        scenery, it’s not hard to see why! <br>
+                        If you want to get married in Mexico, there is nothing legal or technical
+                        stopping you from doing so. Either you want a symbolic ceremony or get
+                        legally married, we can help you to plan ahead
                     </p>
                     <!-- <p>
                         The best moments captured in one single image
@@ -37,33 +41,91 @@
             </div>
         </div>
         <div class="row">
+            <div class="col-md-6" style="margin-top:2rem; margin-bottom:3rem;">
+                <h2 style="color: #b6b8a9;">SYMBOLIC WEDDINGS</h2>
+                <p style="font-family: 'Montserrat';">
+                    Symbolic ceremonies are fun and easy to orchestrate. You can hire a
+                    Minister in Mexico and your ceremony can be customized as much as you
+                    want. The ceremony can be religious, non-religious, or spiritual. You can
+                    have readings or poems read and your friends or relatives can participate
+                    in your wedding. <br>
+                    No blood test, no witnesses, no paperwork is required. <br>
+                    Also, you can add different wedding ceremonies such as sand ceremony,
+                    unity candle, loving cup, lasso, 13 coins, veil ceremony, wine and box, etc.
+                </p>
+            </div>
+            <div class="col-md-6" style="margin-top:2rem; margin-bottom:3rem;">
+                <h2 style="color: #b6b8a9;">LEGAL WEDDINGS</h2>
+                <p style="font-family: 'Montserrat';">
+                    Legal Weddings in Mexico are legal in the US and Canada but in order to
+                    do so, you must meet some requirements. <br>
+                    To start you will need to complete a marriage request form, bring your
+                    birth certificate and passport. A blood test taken in Mexico prior to the
+                    wedding and 4 witnesses with an ID will also be some requirements to get
+                    legally married in Mexico. Ask us for more information!
+                </p>
+
+            </div>
+        </div>
+        <div class="row">
             <!-- 3 Columns -->
             <div class="col-12 col-md-4 col-lg-4" style="margin-bottom: 45px; margin-top:20px">
                 <div class="image">
-                    <img class="img-responsive" src="/assets/images/servicios/16.jpg" alt="baby-tigger">
+                    <img class="img-responsive" src="/assets/images/servicios/16.jpg?=1" alt="baby-tigger">
                 </div>
             </div>
             <div class="col-12 col-md-4 col-lg-4" style="margin-bottom: 45px; margin-top:20px">
                 <div class="image">
-                    <img class="img-responsive" src="/assets/images/servicios/17.jpg" alt="baby-tigger">
+                    <img class="img-responsive" src="/assets/images/servicios/18.jpg?=1" alt="baby-tigger">
                 </div>
             </div>
             <div class="col-12 col-md-4 col-lg-4" style="margin-bottom: 45px; margin-top:20px">
                 <div class="image">
-                    <img class="img-responsive" src="/assets/images/servicios/18.jpg" alt="baby-tigger">
+                    <img class="img-responsive" src="/assets/images/servicios/17.jpg?=1" alt="baby-tigger">
                 </div>
             </div>
             <!-- 2 Columns -->
             <div class="col-12 col-md-6 col-lg-6" style="margin-bottom: 45px; margin-top:20px">
                 <div class="image">
-                    <img class="img-responsive" src="/assets/images/servicios/19.jpg" alt="baby-tigger">
+                    <img class="img-responsive" src="/assets/images/servicios/19.jpg?=1" alt="baby-tigger">
                 </div>
             </div>
             <div class="col-12 col-md-6 col-lg-6" style="margin-bottom: 45px; margin-top:20px">
                 <div class="image">
-                    <img class="img-responsive" src="/assets/images/servicios/20.jpg" alt="baby-tigger">
+                    <img class="img-responsive" src="/assets/images/servicios/20.jpg?=1" alt="baby-tigger">
                 </div>
             </div>
+
+
+            <!-- 3 Columns -->
+            <div class="col-12 col-md-4 col-lg-4" style="margin-bottom: 45px; margin-top:20px">
+                <div class="image">
+                    <img class="img-responsive" src="/assets/images/servicios/49.jpg?=1" alt="baby-tigger">
+                </div>
+            </div>
+            <div class="col-12 col-md-4 col-lg-4" style="margin-bottom: 45px; margin-top:20px">
+                <div class="image">
+                    <img class="img-responsive" src="/assets/images/servicios/51.jpg?=1" alt="baby-tigger">
+                </div>
+            </div>
+            <div class="col-12 col-md-4 col-lg-4" style="margin-bottom: 45px; margin-top:20px">
+                <div class="image">
+                    <img class="img-responsive" src="/assets/images/servicios/50.jpg?=1" alt="baby-tigger">
+                </div>
+            </div>
+            <!-- 2 Columns -->
+            <div class="col-12 col-md-6 col-lg-6" style="margin-bottom: 45px; margin-top:20px">
+                <div class="image">
+                    <img class="img-responsive" src="/assets/images/servicios/52.jpg?=1" alt="baby-tigger">
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-6" style="margin-bottom: 45px; margin-top:20px">
+                <div class="image">
+                    <img class="img-responsive" src="/assets/images/servicios/53.jpg?=1" alt="baby-tigger">
+                </div>
+            </div>
+
+
         </div>
 
     </div>
