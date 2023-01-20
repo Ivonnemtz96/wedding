d3543aa1657acc6876facf3f06003a6a1ca0a77f
Ivonnemtz96 <ivonnemtz96@outlook.es>
2023-01-20 08:52:59 -0700

    Cambios bajados del servidor

---
+++ carrucel/index.php
@@ -0,0 +1,392 @@
+<!-- start of hero -->
+<section class="hero-slider hero-style-1">
+    <div class="swiper-container">
+        <div class="slide-main-text">
+            <!-- <div class="container">
+                <div class="slide-title">
+                    <h2>Dorian & Blit</h2>
+                </div>
+                <div class="wedding-date">
+                    <span>14 Feb 2020</span>
+                </div>
+                <div class="clearfix"></div>
+                <div class="count-down-clock">
+                    <div id="clock"></div>
+                </div>
+            </div> -->
+            <div class="pattern">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <div class="slide-inner slide-bg-image" data-background="/assets/images/slider/slide-1.jpg"></div>
+            </div>
+
+            <div class="swiper-slide">
+                <div class="slide-inner slide-bg-image" data-background="/assets/images/slider/slide-2.jpg"></div>
+            </div>
+
+            <div class="swiper-slide">
+                <div class="slide-inner slide-bg-image" data-background="/assets/images/slider/slide-3.jpg"></div>
+            </div><!-- end swiper-slide -->
+        </div>
+        <!-- end swiper-wrapper -->
+
+        <!-- swipper controls -->
+        <div class="swiper-pagination"></div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+    </div>
+</section>
+<!-- end of hero slider -->
+
+
+<!-- start services-section -->
+<section hidden class="services-section section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
+                <div class="section-title">
+                    <h2>About</h2>
+                    <p>
+                        Weddings at Cabo by Susana Flores, is a premier company based in Los
+                        Cabos that specializes in designing beautifully styled events. No matter
+                        what type of celebration you are planning, we can help you to create the
+                        best ambiance for you and your guests. <br>
+                        A night to be remembered as an unforgettable one!
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div> <!-- end container -->
+</section>
+<!-- end services-section -->
+
+<!-- start couple-section -->
+<section class="couple-section section-padding p-t-4">
+    <div class="container">
+        <div class="row">
+            <div class="col col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
+                <div class="section-title">
+                    <h2>Weddings at Cabo</h2>
+                    <!-- <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque temporibus natus suscipit.
+                        Praesentium minus velit reiciendis officiis commodi voluptatem numquam!
+                    </p> -->
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col col-lg-10 col-lg-offset-1">
+                <div class="couple-area">
+                    <div class="couple-row clearfix">
+                        <div class="img-holder">
+                            <img src="/assets/images/couple/img-1.jpg" alt>
+                        </div>
+                        <div class="details">
+                            <div class="inner">
+                                <h2 style="margin-top: 4rem;">About</h2>
+                                <!-- <h4>Hi Dear, I am Dorian Wiliam.</h4> -->
+                                <p>
+                                    Weddings at Cabo by Susana Flores, is a premier company based in Los
+                                    Cabos that specializes in designing beautifully styled events. No matter
+                                    what type of celebration you are planning, we can help you to create the
+                                    best ambiance for you and your guests. A night to be remembered as an
+                                    unforgettable one!
+                                </p>
+                                <ul class="social-links">
+                                    <li><a href="#"><i class="ti-facebook"></i></a></li>
+                                    <li><a href="#"><i class="ti-instagram"></i></a></li>
+                                    <!-- <li><a href="#"><i class="ti-twitter-alt"></i></a></li>
+                                    <li><a href="#"><i class="ti-linkedin"></i></a></li>
+                                    <li><a href="#"><i class="ti-pinterest"></i></a></li> -->
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="couple-row last-row clearfix">
+                        <div class="details">
+                            <div class="inner">
+                                <h2>Philosophy</h2>
+                                <!-- <h4>Hi Dear, I am Blit Jhon.</h4> -->
+                                <p>
+                                    Our philosophy is that we conceptualize, plan and produce your dream
+                                    ambiance. Whether your affair is intimate and simple, extravagant or
+                                    luxurious, we can create experiences that will last a lifetime.
+                                </p>
+                                <ul class="social-links">
+                                    <li><a href="#"><i class="ti-facebook"></i></a></li>
+                                    <li><a href="#"><i class="ti-instagram"></i></a></li>
+                                    <!-- <li><a href="#"><i class="ti-twitter-alt"></i></a></li>
+                                    <li><a href="#"><i class="ti-linkedin"></i></a></li>
+                                    <li><a href="#"><i class="ti-pinterest"></i></a></li> -->
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="img-holder">
+                            <img src="/assets/images/couple/img-2.jpg" alt>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- end container -->
+</section>
+<!-- end couple-section -->
+
+<!-- start invitation-section -->
+<section class="invitation-section section-padding" style="padding: 0;">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
+                <div class="section-title">
+                    <h2>Services</h2>
+                    <p>
+                        A Weddings at Cabo event is a vision of beauty - not our vision, but YOUR vision. From the
+                        custom
+                        menu to the décor to the entertainment, our consultants work with you to achieve the event of
+                        your
+                        dreams. For us, the design is in the details, from your favorite china to your passion for food,
+                        the
+                        only limit is your imagination.
+                    </p>
+                </div>
+            </div>
+
+            <div class="col col-xs-12">
+
+                <iframe style="border: none;" width="100%" height="500px"
+                    src="https://weddings.bananagroup.mx/carrucel/index.html">
+                </iframe>
+
+            </div>
+        </div>
+    </div> <!-- end container -->
+</section>
+
+<!-- start invitation-section -->
+<section class="invitation-section section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col col-xs-12">
+                <div class="invitation-box">
+                    <div class="left-vec"></div>
+                    <div class="right-vec"></div>
+                    <div class="inner">
+                        <h2>Save the Date</h2>
+                        <span>This is your time to eat, drink, and be married!.</span>
+                        <h3>Lorem, ipsum dolor.</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        </p>
+                        <a href="#" class="theme-btn" id="scroll">Contact now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- end container -->
+</section>
+<!-- end invitation-section -->
+
+
+
+
+
+
+<!-- start gallery-section -->
+
+<section class="gallery-section section-padding p-t-0" >
+    <div class="container" >
+        <div class="row">
+            <div class="col col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
+                <div class="section-title">
+                    <h2>Captured Moments</h2>
+                    <p>
+                        The best moments captured in one single image
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col col-xs-12" id="galeria">
+                <div class="gallery-container gallery-fancybox masonry-gallery">
+                    <div class="grid grid-item">
+                        <a href="/assets/images/gallery/img-1.jpg" class="fancybox" data-fancybox-group="gall-1">
+                            <img src="/assets/images/gallery/img-1.jpg" alt class="img img-responsive">
+                        </a>
+                    </div>
+                    <div class="grid grid-item">
+                        <a href="/assets/images/gallery/img-2.jpg" class="fancybox" data-fancybox-group="gall-1">
+                            <img src="/assets/images/gallery/img-2.jpg" alt class="img img-responsive">
+                        </a>
+                    </div>
+                    <div class="grid grid-item">
+                        <a href="/assets/images/gallery/img-3.jpg" class="fancybox" data-fancybox-group="gall-1">
+                            <img src="/assets/images/gallery/img-3.jpg" alt class="img img-responsive">
+                        </a>
+                    </div>
+                    <div class="grid grid-item">
+                        <a href="/assets/images/gallery/img-4.jpg" class="fancybox" data-fancybox-group="gall-1">
+                            <img src="/assets/images/gallery/img-4.jpg" alt class="img img-responsive">
+                        </a>
+                    </div>
+                    <div class="grid grid-item width2">
+                        <a href="/assets/images/gallery/img-5.jpg" class="fancybox" data-fancybox-group="gall-1">
+                            <img src="/assets/images/gallery/img-5.jpg" alt class="img img-responsive">
+                        </a>
+                    </div>
+                    <div class="grid grid-item width2">
+                        <a href="/assets/images/gallery/img-7.jpg" class="fancybox" data-fancybox-group="gall-1">
+                            <img src="/assets/images/gallery/img-7.jpg" alt class="img img-responsive">
+                        </a>
+                    </div>
+
+                    <div class="grid grid-item width2">
+                        <a href="/assets/images/gallery/img-6.jpg" class="fancybox" data-fancybox-group="gall-1">
+                            <img src="/assets/images/gallery/img-6.jpg" alt class="img img-responsive">
+                        </a>
+                    </div>
+
+                    <!-- <div class="grid grid-item width2">
+                        <a href="https://www.youtube.com/embed/XSGBVzeBUbk?autoplay=1" data-type="iframe"
+                            class="video-play-btn">
+                            <img src="/assets/images/gallery/img-6.jpg" alt class="img img-responsive">
+                            <i class="ti-control-play"></i>
+                        </a>
+                    </div> -->
+                </div>
+            </div>
+        </div> <!-- end row -->
+
+    </div> <!-- end container -->
+</section>
+<!-- end gallery-section -->
+
+
+<!-- start event-section -->
+<section class="event-section section-padding p-t-0">
+    <div class="top-area">
+        <h2>We help you make your dream come true</h2>
+        <!-- <p class="date">14 Feb 2023</p> -->
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col col-xs-12">
+                <div class="event-grids clearfix">
+                    <div class="grid">
+                        <h3>Address</h3>
+                        <p>Monday to Saturday: <br> 9:00 am - 8:00 pm <br>Cabo San Lucas</p>
+                        <a href="https://goo.gl/maps/jVdhUZXgJkzRgVvL7" target="_blank">See location</a>
+                    </div>
+                    <div class="grid">
+                        <h3>Email</h3>
+                        <p>You can write to us at</p>
+                        <p>
+                            <a href="mailto:info@weddingsatcabo.com">info@weddingsatcabo.com</a>
+                            <br><br>
+                        </p>
+                    </div>
+                    <div class="grid">
+                        <h3>Phone</h3>
+                        <p>Contact us at</p>
+                        <p>
+                            <a href="tel:1234567890">1234567890</a>
+                            <br><br>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- end container -->
+</section>
+<!-- end event-section -->
+
+
+<!-- start contact-section -->
+<section class="contact-section section-padding p-t-0" id="rsvp">
+    <div class="container" id="contact">
+        <div class="row">
+            <div class="col col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
+                <div class="section-title">
+                    <p>Let’s Meet</p>
+                    <h2>MAKE AN INQUIRY</h2>
+
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col col-lg-10 col-lg-offset-1">
+                <div class="contact-form">
+                    <form id="rsvp-form" class="form validate-rsvp-form row" method="post">
+                        <div class="col col-sm-6">
+                            <input type="text" name="name" class="form-control" placeholder="Your Name*">
+                        </div>
+
+                        <div class="col col-sm-6">
+                            <input type="text" name="name" class="form-control" placeholder="Fiance/Business name*">
+                        </div>
+
+                        <div class="col col-sm-6">
+                            <input type="text" name="name" class="form-control" placeholder="Phone*">
+                        </div>
+
+                        <div class="col col-sm-6">
+                            <input type="email" name="email" class="form-control" placeholder="Your Email*">
+                        </div>
+
+
+                        <div class="col col-sm-6">
+                            <select class="form-control" name="events">
+                                <option disabled selected>Type of event*</option>
+                                <option>Symbolic Wedding</option>
+                                <option>Legal Wedding</option>
+                            </select>
+                        </div>
+
+                        <div class="col col-sm-6">
+                            <input type="text" name="email" class="form-control" placeholder="Preferred date*">
+                        </div>
+
+                        <div class="col col-sm-6">
+                            <input type="email" name="email" class="form-control" placeholder="Guest Count*">
+                        </div>
+
+                        <div class="col col-sm-6">
+                            <select class="form-control" name="events">
+                                <option disabled selected>Target Budget*</option>
+                                <option>$40,000 and over</option>
+                                <option>$30,000 and under</option>
+                                <option>$10,000 and under</option>
+                            </select>
+                        </div>
+
+                        <div class="col col-sm-12">
+                            <input type="email" name="email" class="form-control" placeholder="Desired Venue*">
+                        </div>
+
+                        <div class="col col-sm-12">
+                            <textarea class="form-control" name="notes" placeholder="Tell Us a little about your celebration*"></textarea>
+                        </div>
+                        <div class="col col-sm-12 submit-btn">
+                            <button type="submit" class="theme-btn">Send</button>
+                            <div id="loader">
+                                <i class="ti-reload"></i>
+                            </div>
+                        </div>
+                        <div class="col col-md-12 success-error-message">
+                            <div id="success">Thank you</div>
+                            <div id="error"> Error occurred while sending email. Please try again later. </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div> <!-- end container -->
+</section>
+<!-- end contact-section -->
