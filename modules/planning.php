b57139ca51313db9f0d166a90a0c84bf8673200a
Ivonnemtz96 <ivonnemtz96@outlook.es>
2023-01-25 12:45:33 -0700

    Alineación entre parráfos

--- modules/planning.php
+++ modules/planning.php
@@ -22,7 +22,7 @@
 <!-- end page-title -->
 <section class="portafolio">
     <div class="container" style="margin-top: 5rem;">
-        <div class="row">
+        <div hidden class="row">
             <div class="col col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
                 <div class="section-title">
                     <h2 style="color: #b6b8a9;">OUR AMAZING WORK</h2>
@@ -62,6 +62,48 @@
                 </div>
             </div>
         </div>
+
+        <div class="row">
+            <div class="col-md-6" style="margin-top:2rem; margin-bottom:3rem;">
+                <h2 style="color: #b6b8a9;">OUR AMAZING WORK</h2>
+                <p style="font-family: 'Montserrat'; font-size:1rem;margin: 0;">
+                    This is your time to eat, drink, and be married!
+                    While you enjoy the festivities, we will take care of the details. Our
+                    specialized planner will work every step of the way to make your wedding a
+                    reality. <br>
+                    Feel free to request a free initial consultation. This will help us to make sure
+                    we understand your vision and can work together to make your wedding
+                    dream come true! <br>
+                    We will be there every step of the way helping you to select the perfect
+                    venue, the right vendors and to customize your food and beverage menus.
+                    We are ready to create an unforgettable night for you and your loved ones!
+                    <br><br>
+                </p>
+            </div>
+            <div class="col-md-6" style="margin-top:2rem; margin-bottom:3rem;">
+                <h2 style="color: #b6b8a9;">SERVICES INCLUDED</h2>
+                <p style="font-family: 'Montserrat'; font-size:1rem; margin: 0;">
+                <ul style="list-style: none;">
+                    <li style="color: #444;">* Meetings before the wedding to be used at your leisure.</li>
+                    <li style="color: #444;">* If you are planning to visit Los Cabos soon, we will help you to
+                        create
+                        an agenda that will include: touring locations, vendor meetings, cake
+                        and catering tastings, floral selection, furniture, decor, etc.</li>
+                    <li style="color: #444;">* Create a detailed timeline for your event</li>
+                    <li style="color: #444;">* Budget management</li>
+                    <li style="color: #444;">* Organization of rehearsal dinner, reservations, etc.</li>
+                    <li style="color: #444;">* Full design and styling concept based on your vision.</li>
+                    <li style="color: #444;">* Coordinate the wedding party, all wedding day vendors, and
+                        orchestrate the wedding ceremony.
+                    </li>
+
+                </ul>
+                </p>
+
+            </div>
+        </div>
+
+
         <div class="row">
             <!-- 3 Columns -->
             <div class="col-12 col-md-4 col-lg-4" style="margin-bottom: 45px; margin-top:20px">
